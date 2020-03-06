8ab26fc5ad7f777d4c1864453989d19a3eff494a
Robert Farfsing <robertfarfsing@gmail.com>
2020-03-06 11:59:46 -0500

    Add project files.

---
+++ practiceweb/Data/PTestDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using practiceweb.Models;
+
+namespace practiceweb.Data
+{
+    public class PTestDbContext : DbContext
+    {
+        public PTestDbContext (DbContextOptions<PTestDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<practiceweb.Models.Major> Major { get; set; }
+    }
+}
